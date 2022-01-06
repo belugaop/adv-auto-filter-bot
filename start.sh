6f6320068e2cbca7ab96aaf5676336f992f91db2
belugaop <96910602+belugaop@users.noreply.github.com>
2022-01-06 14:44:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/belugaop/filter-for-tg-v3.git /filter-for-tg-v3
+  git clone https://github.com/belugaop/adv-auto-filter-bot.git /adv-auto-filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /filter-for-tg-v3
 fi
-cd /filter-for-tg-v3
+cd /adv-auto-filter-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
